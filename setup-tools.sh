3c30b6451f08fddb865c9b431bd5219ec5ed732b
InersIn <38537813+InersIn@users.noreply.github.com>
2025-05-03 23:37:29 +0700

    Create setup-tools.sh
    Install and setup GDB extension

---
+++ setup-tools.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+echo "[+] Setup pwndbg"
+git clone https://github.com/pwndbg/pwndbg
+cd pwndbg
+./setup.sh
+cd ..
+mv pwndbg ~/pwndbg-src
+echo "source ~/pwndbg-src/gdbinit.py" > ~/.gdbinit_pwndbg
+
+echo "[+] Setup peda"
+git clone https://github.com/longld/peda.git ~/peda
+
+echo "[+] Setup gef"
+wget -q -O ~/.gdbinit-gef.py https://github.com/hugsy/gef/raw/master/gef.py
+
+echo "[+] Combine All"
+
+cat << EOF >> ~/.gdbinit
+define init-peda
+source ~/peda/peda.py
+end
+document init-peda
+Initializes the PEDA (Python Exploit Development Assistant for GDB) framework
+end
+
+define init-pwndbg
+source ~/.gdbinit_pwndbg
+end
+document init-pwndbg
+Initializes PwnDBG
+end
+
+define init-gef
+source ~/.gdbinit-gef.py
+end
+document init-gef
+Initializes GEF (GDB Enhanced Features)
+end
+EOF
+
+echo "[+] Setup finished!"
